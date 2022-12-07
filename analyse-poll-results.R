c99e066303fee76b014a6fc965d82b42fb3ed441
Marc <marc@trichotomy.xyz>
2022-12-07 14:19:20 +0100

    poll analysis

---
+++ analyse-poll-results.R
@@ -0,0 +1,27 @@
+library(tidyverse)
+
+polls <- read_excel("data/Polls-per-user-CEU_R_Python_Stata_2022.xlsx") |>
+  slice(-1) |>
+  rename(
+    id = `User ID`,
+    after = starts_with("[AFTER"),
+    before = starts_with("[Start]"),
+    convincing = starts_with("Which was")
+  ) |>
+  select(id, after, before, convincing) |>
+  mutate(convincing = case_when(
+    str_starts(convincing, "Team Stata")  ~ "Stata",
+    str_starts(convincing, "Team R")      ~ "R",
+    str_starts(convincing, "Team Python") ~ "Python"
+  )) |>
+  # Limit to people who answer before and after
+  filter(if_all(c(after, before), ~ !is.na(.)))
+
+flow_plot <- polls |>
+  count(before, after) |>
+  ggplot() + 
+  geom_col(aes(x = before, y = n, fill = before)) + 
+  facet_wrap(~after) + 
+  labs(caption = "Final votes for each lang, broken down by initial (pre-debate) vote.")
+
+ggsave("img/flow_plot_ceu_debate.png", flow_plot)
